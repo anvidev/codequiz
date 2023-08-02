72db00da4c21858fc1e196e6d4d7caeef9e5a021
Andreas Vifert <andreasgylche@gmail.com>
2023-08-02 23:18:03 +0200

    updated prisma to latest (5.1.0)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import "server-only";
 
-declare global {
-  // eslint-disable-next-line no-var, no-unused-vars
-  var cachedPrisma: PrismaClient;
-}
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-export let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = cachedPrisma;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
